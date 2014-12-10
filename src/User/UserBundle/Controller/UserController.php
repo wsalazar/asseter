f9eb4c51a57ecd964afa807d5ee0ed8383f417b9
Will Salazar <was@williamsalazar.com>
2014-12-09 23:29:57 -0500

    downloaded assets and trying to incorporate.

--- src/User/UserBundle/Controller/UserController.php
+++ src/User/UserBundle/Controller/UserController.php
@@ -16,11 +16,16 @@ class UserController extends Controller
         $userLogin->setPasscode('');
 
         $loginForm = $this->createFormBuilder($userLogin)
-                            ->add('email','text')
+                            ->add('email','email')
                             ->add('passcode','password')
-                            ->add('save','submit',['label'=>'Login'])
-                            ->add('register','submit',['label'=>'Register'])
+                            ->add('save','input',['label'=>'Login','attr'=>['class'=>'fa fa-arrow-right']])
+                            ->add('register','input',['label'=>'Register'])
                             ->getForm();
+//        background: none repeat scroll 0 0 #448df6;
+//    border: 3px solid #448df6;
+//    color: #14c65e;
+//    float: left;
+//    margin-right: 10px;
         $loginForm->handleRequest($request);
         return $this->render(
                             'UserUserBundle:User:new.html.twig',
--- src/User/UserBundle/Entity/Login.php
+++ src/User/UserBundle/Entity/Login.php
@@ -10,24 +10,43 @@ namespace User\UserBundle\Entity;
 
 
 class Login {
+    /**
+     * @var $email string
+     */
     protected $email;
+
+    /**
+     * @var $passcode string
+     */
     protected $passcode;
 
+    /**
+     * @return string $email
+     */
     public function getEmail()
     {
         return $this->email;
     }
 
+    /**
+     * @param $email string
+     */
     public function setEmail($email)
     {
         $this->email = $email;
     }
 
+    /**
+     * @return string $passcode
+     */
     public function getPasscode()
     {
         return $this->passcode;
     }
 
+    /**
+     * @param $passcode string
+     */
     public function setPasscode($passcode)
     {
         $this->passcode = $passcode;
